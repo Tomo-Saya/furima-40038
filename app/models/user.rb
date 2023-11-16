baff374b1deca14ad71b28bdc2a6cb536309497f
BigSwan-ALB <sym1015tmy@gmail.com>
2023-11-16 15:25:10 +0900

    ユーザー管理機能(4)

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
     validates :last_name,       presence: true
     validates :first_name,      presence: true
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' } do
     validates :last_name_kana,  presence: true
     validates :first_name_kana, presence: true
   end
