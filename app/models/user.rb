419a5f89cf80cd19e61ed87ac08fc20d3c0842b2
BigSwan-ALB <sym1015tmy@gmail.com>
2023-11-16 12:08:45 +0900

    ユーザー管理機能(2)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,16 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable
+         
   validates :nickname,        presence: true
-  # validates :email,           presence: true, uniqueness: true
-  # validates :password,        presence: true
-  # VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  # validates :password, format: { with: VALID_PASSWORD_REGEX, message:" is invalid. Include both letters and numbers" }
-  # validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'is invalid. Input full-width characters' }
-  # validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'is invalid. Input full-width characters' }
-  # validates :last_name_kana,  presence: true, format: { with: /\A\p{Katakana}+\z/, message: 'is invalid. Input full-width katakana characters' }
-  # validates :first_name_kana, presence: true, format: { with: /\A\p{Katakana}+\z/, message: 'is invalid. Input full-width katakana characters' }
+  validates :email,           presence: true, uniqueness: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: " is invalid. Include both letters and numbers"
+  validates :password, presence: true, format: { with: PASSWORD_REGEX, message: " is invalid. Include both letters and numbers" }
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
-    validates :first_name
-    validates :last_name
-    validates :last_name_kana
-    validates :first_name_kana
+    validates :last_name,      presence: true
+    validates :first_name,       presence: true
+    validates :last_name_kana,  presence: true
+    validates :first_name_kana, presence: true
   end
     validates :birthday,        presence: true
   
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
     email                 {Faker::Internet.email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    last_name { 'てすと' }
+    first_name { 'てすと' }
+    last_name_kana { 'テスト' }
+    first_name_kana { 'テスト' }
+    birthday { Date.new(1990, 1, 1) }
   end
 end
