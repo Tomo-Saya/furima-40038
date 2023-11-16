cb5ed71ec05378778c263f16fd586ee5596f2700
BigSwan-ALB <sym1015tmy@gmail.com>
2023-11-16 15:54:09 +0900

    ユーザー管理機能(5)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-  validates :password, format: { with: PASSWORD_REGEX, message: ' is invalid. Include both letters and numbers' }
+  validates :password, format: { with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' }
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
     validates :last_name,       presence: true
     validates :first_name,      presence: true
